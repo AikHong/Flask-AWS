bcd354bc56c7f11a86472258d3a912e0136f191a
AikHong <aikhongt@gmail.com>
2019-08-05 21:25:44 +0800

    Updated RDS endpoint

--- config.py
+++ config.py
@@ -2,7 +2,13 @@
 # The other elements are the same as used in the tutorial
 # format: (user):(password)@(db_identifier).amazonaws.com:3306/(db_name)
 
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://GCS:GCS368001@treadmilldashboard.cfcjymcgcyn5.ap-southeast-1.rds.amazonaws.com:3306/treadmilldashboard'
+import sqlalchemy as db
+
+#SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://GCS:GCS368001@gymdb.cfcjymcgcyn5.ap-southeast-1.rds.amazonaws.com:3306/gymdb'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://AikHong:password1234@gym2.cfcjymcgcyn5.ap-southeast-1.rds.amazonaws.com:3306/GreatDB'
+#engine = db.create_engine('mysql+pymysql://GCS:GCS368001@gymdb.cfcjymcgcyn5.ap-southeast-1.rds.amazonaws.com:3306/gymdb')
+#connection = engine.connect()
+
 
 # Uncomment the line below if you want to work with a local DB
 #SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
@@ -10,4 +16,4 @@ SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://GCS:GCS368001@treadmilldashboard.cfcj
 SQLALCHEMY_POOL_RECYCLE = 3600
 
 WTF_CSRF_ENABLED = True
-SECRET_KEY = 'dI9kRyu6PoS/EeRblrJhww+RWj88YtuPPmkdQLbk'
+SECRET_KEY = 'U7OApAEgWuML1BAZGhL0FUoNydPqFkPhZlDHZXyv'
